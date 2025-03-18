40b92eda66f1eee58bba21d1ce1be2c0a2e9b8e8
Gippomedont <160245895+Gippomedont@users.noreply.github.com>
2025-03-18 16:29:31 +0400

    Update BookInDepartment.cs

--- Library/Model/BookInDepartment.cs
+++ Library/Model/BookInDepartment.cs
@@ -2,11 +2,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Library.Domain.Model;
 
+/// <summary>
+/// Класс, представляющий связь книги с отделом библиотеки.
+/// </summary>
 public class BookInDepartment
 {
+    /// <summary>
+    /// Идентификатор записи.
+    /// </summary>
     [Key]
-    public required int Id { get; set; } // Идентификатор записи
-    public required int InventoryNumber { get; set; } // Инвентарный номер книги
-    public required int DepartmentId { get; set; } // Идентификатор отдела
-    public required int Quantity { get; set; } // Количество книг
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Инвентарный номер книги.
+    /// </summary>
+    public required int InventoryNumber { get; set; }
+
+    /// <summary>
+    /// Идентификатор отдела, в котором находится книга.
+    /// </summary>
+    public required int DepartmentId { get; set; }
+
+    /// <summary>
+    /// Количество книг данного экземпляра в отделе.
+    /// </summary>
+    public required int Quantity { get; set; }
 }
