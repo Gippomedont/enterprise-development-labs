040e22646e205d4b183d11a02bfe2b39753393f7
Gippomedont <160245895+Gippomedont@users.noreply.github.com>
2025-03-18 16:31:03 +0400

    Update BookIssue.cs

--- Library/Model/BookIssue.cs
+++ Library/Model/BookIssue.cs
@@ -2,16 +2,44 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Library.Domain.Model;
 
+/// <summary>
+/// Класс, представляющий выдачу книги читателю.
+/// </summary>
 public class BookIssue
 {
+    /// <summary>
+    /// Идентификатор выдачи.
+    /// </summary>
     [Key]
-    public required int Id { get; set; } // Идентификатор выдачи
-    public required int InventoryNumber { get; set; } // Инвентарный номер книги
-    public required int ReaderId { get; set; } // Идентификатор читателя
-    public required DateTime IssueDate { get; set; } // Дата выдачи
-    public required int DaysIssued { get; set; } // Количество дней на выдачу
-    public DateTime? ReturnDate { get; set; } // Дата возврата
+    public required int Id { get; set; }
 
-    // Навигационное свойство для связи с книгой
+    /// <summary>
+    /// Инвентарный номер выданной книги.
+    /// </summary>
+    public required int InventoryNumber { get; set; }
+
+    /// <summary>
+    /// Идентификатор читателя, которому выдана книга.
+    /// </summary>
+    public required int ReaderId { get; set; }
+
+    /// <summary>
+    /// Дата выдачи книги.
+    /// </summary>
+    public required DateTime IssueDate { get; set; }
+
+    /// <summary>
+    /// Количество дней, на которые выдана книга.
+    /// </summary>
+    public required int DaysIssued { get; set; }
+
+    /// <summary>
+    /// Дата возврата книги (если книга уже возвращена).
+    /// </summary>
+    public DateTime? ReturnDate { get; set; }
+
+    /// <summary>
+    /// Навигационное свойство для связи с книгой.
+    /// </summary>
     public Book? Book { get; set; }
 }
