30b47c4464d3bea5c3ae9ceb67f69cec4b481406
Gippomedont <160245895+Gippomedont@users.noreply.github.com>
2025-03-18 16:33:34 +0400

    Update Reader.cs

--- Library/Model/Reader.cs
+++ Library/Model/Reader.cs
@@ -2,12 +2,36 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Library.Domain.Model;
 
+/// <summary>
+/// Класс, представляющий читателя библиотеки.
+/// </summary>
 public class Reader
 {
+    /// <summary>
+    /// Идентификатор читателя.
+    /// </summary>
     [Key]
-    public required int Id { get; set; } // Идентификатор читателя
-    public required string FullName { get; set; } // ФИО
-    public required string Address { get; set; } // Адрес
-    public required string Phone { get; set; } // Телефон
-    public required DateTime RegistrationDate { get; set; } // Дата регистрации
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Полное имя читателя (ФИО).
+    /// </summary>
+    public required string FullName { get; set; }
+
+    /// <summary>
+    /// Адрес читателя.
+    /// </summary>
+    public required string Address { get; set; }
+
+    /// <summary>
+    /// Телефон читателя.
+    /// </summary>
+    public required string Phone { get; set; }
+
+    /// <summary>
+    /// Дата регистрации читателя в библиотеке.
+    /// </summary>
+    public required DateTime RegistrationDate { get; set; }
 }
+// Какие ж эти комментарии получаются объёмные...
+// Если будет построковая оплата на работе, обязательно возьму на вооружение)
