e72ae9a9a850f6e8d7a5d26e2c3c1659a7d96c6d
Gippomedont <160245895+Gippomedont@users.noreply.github.com>
2025-03-18 16:31:33 +0400

    Update Department.cs

--- Library/Model/Department.cs
+++ Library/Model/Department.cs
@@ -2,9 +2,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Library.Domain.Model;
 
+/// <summary>
+/// Класс, представляющий отдел библиотеки.
+/// </summary>
 public class Department
 {
+    /// <summary>
+    /// Идентификатор отдела.
+    /// </summary>
     [Key]
-    public required int Id { get; set; } // Идентификатор отдела
-    public required string Name { get; set; } // Название отдела
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Название отдела.
+    /// </summary>
+    public required string Name { get; set; }
 }
